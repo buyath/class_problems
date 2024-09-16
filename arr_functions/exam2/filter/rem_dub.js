6011820e6f4c76ba6d09a3a75521b69c697d2f46
Mohamed Mohideen Thayub <mthayub@freshworks.com>
2024-09-16 01:55:22 -0500

    morning problem solved

---
+++ arr_functions/exam2/filter/rem_dub.js
@@ -0,0 +1,21 @@
+// const numbers = [1, 2, 2, 3, 4, 4, 5];
+// const total = numbers.filter(x =>{
+//     if (x > )
+// })
+// let cars = ["Audi","Bmw","cooper"];
+// for(var i =0; i<cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+var fruits= ["mango","orange","kiwi","grapes","apple"];
+// for(var i =0 ; i<fruits.length; i++){
+//     console.log("fruits of values",fruits[i]);
+
+// }
+
+
+var fruits= ["mango","orange","kiwi","grapes","apple"];
+var n =  fruits.length;
+for(var i = 1; i<=n; i++){
+    console.log(fruits[n-i])
+}
---
+++ arr_functions/exam2/filter/reverse_arr.js
@@ -0,0 +1,4 @@
+var numbers = ["one","two","three","four","five"];
+for(var i = 1; i<=numbers.length; i++){
+    console.log("number of values is",numbers[numbers.length-i])
+}
