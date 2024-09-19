9501f7c88f92f8afcccc2a67ed8595c0cb4c8b7b
Mohamed Mohideen Thayub <mthayub@freshworks.com>
2024-09-19 08:20:41 -0500

    today solved 3 problem without square

---
+++ date_19/even_sum.js
@@ -0,0 +1,10 @@
+var a = 5;
+var b  = 10;
+var sum = 0;
+for(var i =a; i<=b; i++){
+    if (i % 2 ==0){
+        sum +=i;
+    }
+    
+}
+console.log(sum);
---
+++ date_19/space_sep.js
@@ -0,0 +1,5 @@
+var Name = "freshworks,software,academy,thayub";
+var arr = Name.split(',');
+for(var i =0; i<arr.length; i++){
+    console.log(arr[i]);
+}
