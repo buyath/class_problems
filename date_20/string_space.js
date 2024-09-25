7ba350dc0ed76ec0c2eb2a7877c1b6ad9b8e8d8e
Mohamed Mohideen Thayub <mthayub@freshworks.com>
2024-09-25 03:09:04 -0500

    morning problem done

--- date_20/string_space.js
+++
@@ -1,6 +0,0 @@
-var arr = [1,2,3,4,5];
-var str = "";
-for(var i = 0; i<=str.length; i++){
-    str = str + arr[i] + " ";
-}
-console.log(str);
---
+++ date_25/another_arr_rev.js
@@ -0,0 +1,9 @@
+var arr = [1,2,3,4,5,6];
+var arr2 =[]
+for(var i =1; i<=arr.length; i++){
+    arr2.push(arr[arr.length-i]);
+}
+console.log(arr2);
+
+
+
