3d8eeb0ebeb65c0da1dd24f125cf402102844062
Mohamed Mohideen Thayub <mthayub@freshworks.com>
2024-09-26 07:14:49 -0500

    while to for converted odd

--- date_26/generate_odd.js
+++ date_26/generate_odd.js
@@ -1,9 +1,16 @@
-let n = 10;
-let i = 1; 
-let odd = 1;
-while(i <=n){
-    console.log(odd);
-    odd +=2;
-    i=i+1;
+let num = 4;
+let odd2 = 1;
+for(var j = 1; j<=num; j++ ){
+    console.log(odd2);
+    odd2+=2;
 }
 
+var str = ""
+var odd = 1;
+for(var i =1; i<=4; i++){
+    str =  str +odd + " ";
+    odd = odd + 2;
+    
+}
+// 1 3 5 7 
+console.log(str);
